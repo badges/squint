50b3f5b5bdb4b2930e7bda679df6618fd113a8e3
Caleb Cartwright <caleb.cartwright@outlook.com>
2020-07-18 14:59:18 -0500

    feat: add server scaffold

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+mod server;
+use server::{start_server, GenericServerError};
+
+const DEFAULT_PORT: u16 = 3001;
+
+#[tokio::main]
+async fn main() -> Result<(), GenericServerError> {
+    let port =
+        std::env::var("PORT").map_or(DEFAULT_PORT, |p| p.parse::<u16>().unwrap_or(DEFAULT_PORT));
+    let socket_address: std::net::SocketAddr = ([0, 0, 0, 0], port).into();
+    start_server(socket_address).await?;
+    Ok(())
 }
