c43dd82bc71edf6666cc59d6a646cb6e34b2fafa
Caleb Cartwright <caleb.cartwright@outlook.com>
2020-08-01 16:54:36 -0500

    fix: cleanup server routing

--- src/server.rs
+++ src/server.rs
@@ -6,28 +6,27 @@ pub type GenericServerError = Box<dyn std::error::Error + Send + Sync>;
 
 const SVG_BASE_URL: &str = "https://img.shields.io";
 const HEALTH_CHECK_BODY: &str = concat!(
-    r#"{{"status":"pass","version":""#,
+    r#"{"status":"pass","version":""#,
     env!("CARGO_PKG_VERSION"),
-    r#""}}"#
+    r#""}"#
 );
 
-async fn route(req: Request<Body>) -> Result<Response<Body>, GenericServerError> {
+async fn route(req: Request<Body>) -> Result<Response<Body>, hyper::http::Error> {
     match (req.method(), req.uri().path()) {
-        (&Method::GET, "/") => Ok(Response::builder()
+        (&Method::GET, "/") => Response::builder()
             .status(301)
             .header(hyper::header::LOCATION, SVG_BASE_URL)
-            .body(Body::empty())
-            .unwrap()),
+            .body(Body::empty()),
         // TODO: This should probably incorporate a check of connectivity to the upstream SVG provider
         // https://tools.ietf.org/html/draft-inadarei-api-health-check-03#section-3
-        (&Method::GET, "/health") => Ok(Response::builder()
+        (&Method::GET, "/health") => Response::builder()
             .status(200)
-            .body(Body::from(HEALTH_CHECK_BODY))
-            .unwrap()),
+            .header(hyper::header::CONTENT_TYPE, "application/json")
+            .body(Body::from(HEALTH_CHECK_BODY)),
         // (&Method::GET, _) => rasterize(req, http_client.to_owned()).await,
         // GET is the only supported HTTP Verb at this time, and a GET request with an invalid badge route
         // will be handled by the above arm with a 404 response code. This arm just handles unsupported verbs.
-        (_, _) => Ok(Response::builder().status(405).body(Body::empty()).unwrap()),
+        (_, _) => Response::builder().status(405).body(Body::empty()),
     }
 }
 
