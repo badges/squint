ee2a140738a95a24a43f13163a819907f8549024
chris48s <git@chris-shaw.dev>
2022-11-27 20:18:29 +0000

    store test cases in json array
    this will make it easier to test URLs with ?query=params

--- visual_regression_testing/compare.sh
+++ visual_regression_testing/compare.sh
@@ -8,16 +8,18 @@ mkdir -p test_images
 rm -rf diffs
 mkdir -p diffs
 
+readarray -t test_cases < <(jq -c '.[]' test_cases.json)
+
 # from this point on, continue even if something fails
 # we want to continue testing further reference_images, even if one fails
 set +e
 
 exitcode=0
-for f in reference_images/*
-do
-    filename="$(basename "$f")"
+for test_case in "${test_cases[@]}"; do
+    url=$(jq -r '.url' <<< "$test_case")
+    filename=$(jq -r '.filename' <<< "$test_case")
     echo "Testing: $filename";
-    curl "http://localhost:3001/badge/$filename" --silent -o "test_images/$filename"
+    curl "$url" --silent -o "test_images/$filename"
     if ./node_modules/.bin/pixelmatch "test_images/$filename" "reference_images/$filename" "diffs/$filename"; then
         # if the images were exactly the same, discard the diff
         rm "diffs/$filename"
